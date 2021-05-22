04cdac6b0c92a728fd18d9f56621134d0d89c1d3
m-dwyer <13535330+m-dwyer@users.noreply.github.com>
2021-05-22 18:43:41 +1000

    Initial commit

---
+++ init.el
@@ -0,0 +1,164 @@
+(setq inhibit-startup-message t)
+
+(scroll-bar-mode -1) ; Disable visible scrollbar
+(tool-bar-mode -1) ; Disable the toolbar
+(tooltip-mode -1) ; Disable tooltips
+(set-fringe-mode 10) ; Give some breathing space
+
+;(menu-bar-mode -1) ; Disable the menu bar
+
+;; Set up the visible bell
+(setq visible-bell t)
+
+;; Font
+(set-face-attribute 'default nil :font "Fira Mono" :height 140)
+
+;; Make ESC quit prompts
+(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
+
+;; Initialize package sources
+(require 'package)
+
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+			 ("org" . "https://orgMode.org/elpa/")
+			 ("elpa" . "https://elpa.gnu.org/packages/")))
+
+(package-initialize)
+(unless package-archive-contents
+  (package-refresh-contents))
+
+;; Initialize use-package on non-linux platforms
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+
+(require 'use-package)
+(setq use-package-always-ensure t)
+
+;; Could be useful for showing commands entered in a separate buffer
+;; (use-package command-log-mode)
+
+(use-package diminish)
+
+;; completion frameworks - helm, ivy -- a bit different
+;; let's go with ivy!
+;; we'll get auto-complete on M-x, C-x f (find file) and others.
+;; TODO: investigate :diminish, to remove minor-mode for ivy from the modeline!
+
+(use-package ivy
+  :diminish
+  :config
+  (ivy-mode 1))
+
+;; counsel - install and set bindings for details on key bindings
+;; also required for ivy-rich below
+(use-package counsel
+  :bind (("M-x" . counsel-M-x)
+	 ("C-x b" . counsel-ibuffer)
+	 ("C-x C-f" . counsel-find-file)
+	 :map minibuffer-local-map
+	 ("C-r" . 'counsel-minibuffer-history)))
+
+;; ivy-rich - extend ivy
+(use-package ivy-rich
+  :init
+  (ivy-rich-mode 1))
+
+;; counsel package uses ivy to extend completion, so  we should install this..
+
+;; Themes
+
+;; M-x all-the-icons-install-fonts
+(use-package all-the-icons)
+
+(use-package doom-modeline
+  :ensure t
+  :init (doom-modeline-mode 1)
+  :custom (doom-modeline-height 15))
+
+(use-package doom-themes)
+
+(load-theme 'doom-one t)
+
+;; fix this!
+(setq doom-modeline-height 15)
+
+;; Line numbers
+(column-number-mode)
+(global-display-line-numbers-mode t)
+
+;; Disable line numbers for some modes
+(dolist (mode '(org-mode-hook
+		term-mode-hook
+		eshell-mode-hook
+		shell-mode-hook))
+  (add-hook mode (lambda () (display-line-numbers-mode 0))))
+
+;; Rainbow delimiters!
+(use-package rainbow-delimiters
+  :hook (prog-mode . rainbow-delimiters-mode))
+
+;; which-key (helper) 
+(use-package which-key
+  :init (which-key-mode)
+  :diminish which-key-mode
+  :config
+  (setq which-key-idle-delay 0.3))
+
+;; helpful - more details in better formatted help screen
+(use-package helpful
+  :custom
+  (counsel-describe-function-function #'helpful-callable)
+  (counsel-describe-variable-function #'helpful-variable)
+  :bind
+  ([remap describe-function] . counsel-describe-function)
+  ([remap describe-command] . helpful-command)
+  ([remap describe-variable] . counsel-describe-variable)
+  ([remap describe-key] . helpful-key))
+
+
+;; Key bindings!
+
+;; Make ESC quit prompts
+(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
+
+;; general package to define keys more precisely
+(use-package general
+  :config
+  (general-create-definer md/leader-keys
+    :keymaps '(normal insert visual emacs)
+    :prefix "SPC"
+    :global-prefix "C-SPC")
+  (md/leader-keys
+   "t" '(:ignore t :which-key "toggles")
+   "tt" '(counsel-load-theme :which-key "choose theme")))
+  
+(general-define-key
+ "C-M-j" 'counsel-switch-buffer)
+
+(defun md/evil-hook ()
+  (dolist (mode '(custom-mode
+		  eshell-mode
+		  git-rebase-mode
+		  term-mode))
+    (add-to-list 'evil-emacs-state-modes mode)))
+
+
+;; Setup evil mode!
+(use-package evil
+  :init
+  (setq evil-want-integration t) 
+  (setq evil-want-keybinding nil)
+  (setq evil-want-C-u-scroll t) ;; replaces universal argument key binding
+  (setq evil-want-C-i-jump nil)
+  :hook (evil-mode . md/evil-hook)
+  :config
+  (evil-mode 1)
+  (define-key evil-insert-state-map (kbd "C-g") 'evil-normal-state)
+  (define-key evil-insert-state-map (kbd "C-h") 'evil-delete-backward-char-and-join)
+
+  ;; Use visual line motions even outside of visual-line-mode buffers
+  (evil-global-set-key 'motion "j" 'evil-next-visual-line)
+  (evil-global-set-key 'motion "k" 'evil-previous-visual-line)
+
+  (evil-set-initial-state 'messages-buffer-mode 'normal)
+  (evil-set-initial-state 'dashboard-mode 'normal))

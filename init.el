41a36ac850c6a178575a5cc9249d78914fbfd471
m-dwyer <13535330+m-dwyer@users.noreply.github.com>
2021-05-26 21:33:26 +1000

    Add hydra text scaling key bindings

--- init.el
+++ init.el
@@ -169,3 +169,15 @@
   :after evil
   :config
   (evil-collection-init))
+
+;; Hydra for transient key bindings
+(use-package hydra)
+
+(defhydra hydra-text-scale (:timeout 4)
+  "scale text"
+  ("j" text-scale-increase "in")
+  ("k" text-scale-decrease "out")
+  ("f" nil "finished" :exit t))
+
+(md/leader-keys
+  "ts" '(hydra-text-scale/body :which-key "scale text"))

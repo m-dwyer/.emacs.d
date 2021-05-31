1958981aa053b3a062f17d8b01bf76c8ccb8b038
m-dwyer <13535330+m-dwyer@users.noreply.github.com>
2021-05-31 11:20:40 +1000

    Move to literate config

--- init.el
+++ init.el
@@ -1,34 +1,8 @@
-(setq inhibit-startup-message t)
-
-(scroll-bar-mode -1) ; Disable visible scrollbar
-(tool-bar-mode -1) ; Disable the toolbar
-(tooltip-mode -1) ; Disable tooltips
-(set-fringe-mode 10) ; Give some breathing space
-
-;(menu-bar-mode -1) ; Disable the menu bar
-
-;; Set up the visible bell
-(setq visible-bell t)
-
-;; Font
-(set-face-attribute 'default nil :font "Fira Mono" :height 140)
-
-;; Fixed pitch face
-(set-face-attribute 'fixed-pitch nil :font "Fira Mono" :height 140)
-
-;; Variable pitch face
-(set-face-attribute 'variable-pitch nil :font "Cantarell" :height 200 :weight 'regular)
-
-
-;; Make ESC quit prompts
-(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
-
 ;; Initialize package sources
 (require 'package)
-
 (setq package-archives '(("melpa" . "https://melpa.org/packages/")
-			 ("org" . "https://orgMode.org/elpa/")
-			 ("elpa" . "https://elpa.gnu.org/packages/")))
+                         ("org" . "https://orgMode.org/elpa/")
+                         ("elpa" . "https://elpa.gnu.org/packages/")))
 
 (package-initialize)
 (unless package-archive-contents
@@ -41,89 +15,38 @@
 (require 'use-package)
 (setq use-package-always-ensure t)
 
-;; Could be useful for showing commands entered in a separate buffer
-;; (use-package command-log-mode)
+(setq inhibit-startup-message t)
 
-(use-package diminish)
+(scroll-bar-mode -1) ; Disable visible scrollbar
+(tool-bar-mode -1) ; Disable the toolbar
+(tooltip-mode -1) ; Disable tooltips
+(set-fringe-mode 10) ; Give some breathing space
 
-;; completion frameworks - helm, ivy -- a bit different
-;; let's go with ivy!
-;; we'll get auto-complete on M-x, C-x f (find file) and others.
-;; TODO: investigate :diminish, to remove minor-mode for ivy from the modeline!
+(menu-bar-mode -1) ; Disable the menu bar
 
-(use-package ivy
-  :diminish
-  :config
-  (ivy-mode 1))
-
-;; counsel - install and set bindings for details on key bindings
-;; also required for ivy-rich below
-(use-package counsel
-  :bind (("M-x" . counsel-M-x)
-	 ("C-x b" . counsel-ibuffer)
-	 ("C-x C-f" . counsel-find-file)
-	 :map minibuffer-local-map
-	 ("C-r" . 'counsel-minibuffer-history)))
-
-;; ivy-rich - extend ivy
-(use-package ivy-rich
-  :init
-  (ivy-rich-mode 1))
-
-;; counsel package uses ivy to extend completion, so  we should install this..
-
-;; Themes
-
-;; M-x all-the-icons-install-fonts
-(use-package all-the-icons)
-
-(use-package doom-modeline
-  :ensure t
-  :init (doom-modeline-mode 1)
-  :custom (doom-modeline-height 15))
-
-(use-package doom-themes)
-
-(load-theme 'doom-one t)
-
-;; fix this!
-(setq doom-modeline-height 15)
+;; Set up the visible bell
+(setq visible-bell t)
 
 ;; Line numbers
 (column-number-mode)
-(global-display-line-numbers-mode t)
-
-;; Disable line numbers for some modes
-(dolist (mode '(org-mode-hook
-		term-mode-hook
-		eshell-mode-hook
-		shell-mode-hook))
-  (add-hook mode (lambda () (display-line-numbers-mode 0))))
-
-;; Rainbow delimiters!
-(use-package rainbow-delimiters
-  :hook (prog-mode . rainbow-delimiters-mode))
-
-;; which-key (helper) 
-(use-package which-key
-  :init (which-key-mode)
-  :diminish which-key-mode
-  :config
-  (setq which-key-idle-delay 0.3))
-
-;; helpful - more details in better formatted help screen
-(use-package helpful
-  :custom
-  (counsel-describe-function-function #'helpful-callable)
-  (counsel-describe-variable-function #'helpful-variable)
-  :bind
-  ([remap describe-function] . counsel-describe-function)
-  ([remap describe-command] . helpful-command)
-  ([remap describe-variable] . counsel-describe-variable)
-  ([remap describe-key] . helpful-key))
+(global-display-line-numbers-mode
 
 
-;; Key bindings!
+ ;; Disable line numbers for some modes
+ (dolist (mode '(org-mode-hook
+                 term-mode-hook
+                 eshell-mode-hook
+                 shell-mode-hook))
+   (add-hook mode (lambda () (display-line-numbers-mode 0))))
+
+;; Font
+(set-face-attribute 'default nil :font "Fira Mono" :height 140)
+
+;; Fixed pitch face
+(set-face-attribute 'fixed-pitch nil :font "Fira Mono" :height 140)
+
+;; Variable pitch face
+(set-face-attribute 'variable-pitch nil :font "Cantarell" :height 200 :weight 'regular)
 
 ;; Make ESC quit prompts
 (global-set-key (kbd "<escape>") 'keyboard-escape-quit)
@@ -136,18 +59,18 @@
     :prefix "SPC"
     :global-prefix "C-SPC")
   (md/leader-keys
-   "t" '(:ignore t :which-key "toggles")
-   "tt" '(counsel-load-theme :which-key "choose theme")))
-  
+    "t" '(:ignore t :which-key "toggles")
+    "tt" '(counsel-load-theme :which-key "choose theme")))
+
 (general-define-key
  "C-M-j" 'counsel-switch-buffer)
 
 ;; Put listed modes into evil emacs mode (normal emacs mode)
 (defun md/evil-hook ()
   (dolist (mode '(custom-mode
-		  eshell-mode
-		  git-rebase-mode
-		  term-mode))
+                  eshell-mode
+                  git-rebase-mode
+                  term-mode))
     (add-to-list 'evil-emacs-state-modes mode)))
 
 ;; Setup evil mode!
@@ -163,13 +86,14 @@
   (define-key evil-insert-state-map (kbd "C-g") 'evil-normal-state)
   ;; remove to allow help menu in insert mode
   (define-key evil-insert-state-map (kbd "C-h") 'evil-delete-backward-char-and-join)
-  
+
   ;; Use visual line motions even outside of visual-line-mode buffers
   (evil-global-set-key 'motion "j" 'evil-next-visual-line)
   (evil-global-set-key 'motion "k" 'evil-previous-visual-line)
-  
+
   (evil-set-initial-state 'messages-buffer-mode 'normal)
   (evil-set-initial-state 'dashboard-mode 'normal))
+
 
 ;; evil mode configurations for different emacs modes
 (use-package evil-collection
@@ -177,8 +101,64 @@
   :config
   (evil-collection-init))
 
+(use-package doom-themes)
+
+(load-theme 'doom-one t)
+
+;; M-x all-the-icons-install-fonts
+(use-package all-the-icons)
+
+(use-package doom-modeline
+  :ensure t
+  :init (doom-modeline-mode 1)
+  :custom (doom-modeline-height 15))
+
+(use-package diminish)
+
+;; which-key (helper) 
+(use-package which-key
+  :init (which-key-mode)
+  :diminish which-key-mode
+  :config
+  (setq which-key-idle-delay 0.3))
+
+;; completion frameworks - helm, ivy -- a bit different
+;; let's go with ivy!
+;; we'll get auto-complete on M-x, C-x f (find file) and others.
+;; TODO: investigate :diminish, to remove minor-mode for ivy from the modeline!
+(use-package ivy
+  :diminish
+  :config
+  (ivy-mode 1))
+
+;; counsel - install and set bindings for details on key bindings
+;; also required for ivy-rich below
+(use-package counsel
+  :bind (("M-x" . counsel-M-x)
+         ("C-x b" . counsel-ibuffer)
+         ("C-x C-f" . counsel-find-file)
+         :map minibuffer-local-map
+         ("C-r" . 'counsel-minibuffer-history)))
+
+;; ivy-rich - extend ivy
+(use-package ivy-rich
+  :init
+  (ivy-rich-mode 1))
+
+;; helpful - more details in better formatted help screen
+(use-package helpful
+  :custom
+  (counsel-describe-function-function #'helpful-callable)
+  (counsel-describe-variable-function #'helpful-variable)
+  :bind
+  ([remap describe-function] . counsel-describe-function)
+  ([remap describe-command] . helpful-command)
+  ([remap describe-variable] . counsel-describe-variable)
+  ([remap describe-key] . helpful-key))
+
 ;; Hydra for transient key bindings
-(use-package hydra)
+(use-package hydra
+  :defer t)
 
 (defhydra hydra-text-scale (:timeout 4)
   "scale text"
@@ -188,8 +168,6 @@
 
 (md/leader-keys
   "ts" '(hydra-text-scale/body :which-key "scale text"))
-
-;; Org mode settings
 
 (defun md/org-font-setup ()
   ;; Replace list hyphen with dot
@@ -223,14 +201,14 @@
 
 ;; Improve the bullets
 (use-package org-bullets
-	    :config
-	    (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))))
+  :config
+  (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))))
 
 (setq org-hide-emphasis-markers t)
 
 (defun md/org-mode-visual-fill ()
   (setq visual-fill-column-width 100
-	visual-fill-column-center-text t)
+        visual-fill-column-center-text t)
   (visual-fill-column-mode 1))
 
 (use-package visual-fill-column
@@ -253,3 +231,12 @@
   (setq org-log-into-drawer t)
   (setq org-agenda-files '("~/org/Tasks.org"))
   (md/org-font-setup))
+
+;; Rainbow delimiters!
+(use-package rainbow-delimiters
+  :hook (prog-mode . rainbow-delimiters-mode))
+
+(with-eval-after-load 'org
+  (require 'org-tempo)
+
+  (add-to-list 'org-structure-template-alist '("el" . "src emacs-lisp")))

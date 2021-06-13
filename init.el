73af67da19b97ae4049b220145f249adee8d6d2f
m-dwyer <13535330+m-dwyer@users.noreply.github.com>
2021-06-13 21:00:31 +1000

    Add package auto update

--- init.el
+++ init.el
@@ -15,6 +15,15 @@
 (require 'use-package)
 (setq use-package-always-ensure t)
 
+(use-package auto-package-update
+  :custom
+  (auto-package-update-interval 7)
+  (auto-package-update-prompt-before-update t)
+  (auto-package-update-hide-results t)
+  :config
+  (auto-package-update-maybe)
+  (auto-package-update-at-time "09:00"))
+
 ;; for string utils
 (use-package s)
 
